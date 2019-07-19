47a58b04739e363cf115c987dcfa143a9706b26d
Tanner Eustice <teustice@msn.com>
2019-07-19 11:56:18 -0700

    update authurl

--- src/lib/authUrl.js
+++ src/lib/authUrl.js
@@ -1,3 +1,3 @@
-const authUrl = 'https://my.freshbooks.com/service/auth/oauth/authorize?client_id=8c72f5154be8eb27e1e4ae05313afdb4990e75c94de95901dc677853b2b16a9b&response_type=code&redirect_uri=https://localhost:3000/auth';
+const authUrl = 'https://my.freshbooks.com/service/auth/oauth/authorize?client_id=8c72f5154be8eb27e1e4ae05313afdb4990e75c94de95901dc677853b2b16a9b&response_type=code&redirect_uri=https://fb.cheshirebeane.com/auth';
 
 export default authUrl;
